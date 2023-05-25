d0c1f1cb19bc51cfd05a67cee31ebf1463a4f89f
Yusuf karback <yusufkarback64@gmail.com>
2023-05-25 13:06:50 +0700

    route halaman daftar

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/daftar')
+def daftar_page():
+    return render_template('inner-page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
